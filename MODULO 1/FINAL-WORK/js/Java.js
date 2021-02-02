33379bb28ac463f66834a6270731611649b4f173
Alpi <chimalimiguel@gmail.com>
2021-02-02 07:26:22 +0100

    changes

---
+++ MODULO 1/FINAL-WORK/js/Java.js
@@ -0,0 +1,3 @@
+document.getElementById("name").value="Miguel Ángel";
+document.getElementById("Surname").value="Chimali Cobano";
+document.getElementById("myimage").src = "https://ih1.redbubble.net/image.580418438.6112/st,small,507x507-pad,600x600,f8f8f8.jpg";
