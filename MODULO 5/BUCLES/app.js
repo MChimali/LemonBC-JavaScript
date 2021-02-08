344781386325d82a2c9a7c17a549f79bc001b3d8
Alpi <chimalimiguel@gmail.com>
2021-02-08 23:34:25 +0100

    complete javascript

--- MODULO 5/BUCLES/app.js
+++ MODULO 5/BUCLES/app.js
@@ -36,88 +36,8 @@ for (i = 0; i < ciudades.length; i++){
     console.log (ciudades[i]);
 };
 
-//Veamos algunos ejemplos
-//A continuación mostramos varios objetos con sus propiedades
 
-var reserva1 = { id: 1, price: 10, client: "Caroline" };
-var reserva2 = { id: 2, price: 10, client: "Liam" };
-var reserva3 = { id: 3, price: 10, client: "Andrew" };
-var reserva4 = { id: 4, price: 15, client: "John" };
 
-//metemos todos estos objetos en una lista (array)
-const reservas = [reserva1, reserva2, reserva3, reserva4];
-
-//Si queremos mostrar el precio total de las reservas
-
-let total = 0;
-for (i = 0; i < reservas.length; i++){
-total += reservas[i].price;
-}
-console.log(total);
-
-/*>>>>>>   FOR OF    >>>>>>> Para ir a través de todos y cada uno de los elementos 
-de una lista, nos podemos ahorrar lo de dentro y (a of reservas)*/
-let cost = 0;
-for (a of reservas){
-    cost += a.price;
-}
-console.log(cost);
-
-for (a in reserva1){
-   console.log (reserva1[a])
-}
-
-/*>>>>>> FOR IN >>>>>>> esta se utiliza para ir a través de todas y cada una de las
-propiedades de un objeto.*/
-var cars = {brand: "mercedes", price: 500, puertas: 5};
-
-for (car in cars){
-console.log(car + ": " + cars[car])
-};
-
-//>>>>>  FOR IN >>>>> para arrays >> devuelve el índice y el elemento del array
-var motos = ["kawasaki", "yamaha", "honda"];
-
-for (moto in motos){
-    console.log(moto + ": " + motos[moto]);
-}
-
-/*>>>>>>>>  WHILE  >>>>>>> mientras que una condición se cumpla, se ejecuta un código
-Es igual que el For pero con la inicialización y la postejecución fuera del paréntesis*/
-
-var houses = ["en la playa", "en la montaña", "en el pueblo", "en la ciudad"];
-
-var i = 0;
-while (i < houses.length){
-    console.log("quiero una casa " + houses[i]);
-    i++;
-}
-
-for (i=0; i < houses.length; i++){
-    console.log("quiero una casa " + houses[i]);
-}
-
-////////
-
-var booking1 = { id: 1, price: 10, client: "Caroline" };
-var booking2 = { id: 2, price: 10, client: "Liam" };
-var booking3 = { id: 3, price: 10, client: "Andrew" };
-var booking4 = { id: 4, price: 15, client: "John" };
-
-const bookings = [booking1, booking2, booking3, booking4];
-
-i = 0;
-var encontrado = false;
-while (i < bookings.length && !encontrado){
-    encontrado = bookings[i].id == 3;
-    console.log(encontrado);
-    i++;
-}
-
-/*Con while primero se evalua la condición y luego se ejecuta el 
-bloque de código. Con >>>>> DO WHILE >>>>>> primero se ejecuta el 
-bloque de código y luego se evalua si se sigue ejecutando. El DO WHILE
-siempre se va a ejecutar alguna vez*/
 
 
  
--- MODULO 5/PRÁCTICA/app.js
+++ MODULO 5/PRÁCTICA/app.js
@@ -2,15 +2,14 @@ const carrito = [
 
     {id: 198752, name: "Tinta DJ27 Color", price: 52.95, count: 3, premium: true},
     {id: 75621, name: "Impresora ticketera PRO-201", price: 32.75, count: 2, premium: true},
-    {id: 54657, name: "Caja de rollos de papel para ticketera", price: 5.95, count: 3, premium: false},
-    {id: 3143, name: "Caja de folios DIN-A4 80gr", price: 9.95, count: 2, premium: true}
+    {id: 54657, name: "Caja de rollos de papel para ticketera", price: 5.95, count: 3, premium: true},
+    {id: 3143, name: "Caja de folios DIN-A4 80gr", price: 9.95, count: 2, premium: false}
 
 ];
 
-//Eliminar el producto con id 54657 del carrito de la compra.
-//Al estar este producto eliminado de la lista, éste no se tendrá en cuenta en los cálculos posteriores
-eraseItem(carrito, 198752);
+//    1    // Erase product with id 54657 from the array>>>>>>>>>>>
 
+//Create function to erase a product with a given ID
 function eraseItem(array, id){
 for (i = 0; i < array.length; i++) {
     if (array[i].id === id){
@@ -18,76 +17,157 @@ for (i = 0; i < array.length; i++) {
     }
 }}
 
-//Mostrar el carrito de la compra.
+//We call the function to test if it is working
+//eraseItem(carrito, 198752);
 
-for (i = 0; i < carrito.length; i++) {
-    console.log("Producto: " + (i+1))
-    imprimir (carrito[i]);
-}
 
+//    2    // Show array with all elements>>>>>>>>>>
+
+//Create function to print in console
 function imprimir (products) {
     for (properties in products){
         console.log(properties + ": " + products[properties]);
     }
     console.log("----")
 }
-console.log("");
-console.log("");
 
-//Calcular el total del carrito de la compra (el coste de una línea es precio * cantidad).
-
-let total = 0;
-for (product of carrito){
-    console.log("The total price of " + product.name + " is: " + (product.count*product.price).toFixed(2) + " Euros")
-    total+= product.count*product.price
-}
-console.log("Total: " + total.toFixed(2) + " Euros")
-discount();
-
-//Aplicar 5% descuento si la compra es mayor a 100 euros
-function showDiscount(){
-    console.log("5% descuento por compra superior a 100 euros");
-    console.log("El nuevo total es: " + 0.95*total.toFixed(2));
-}
-
-function discount(){
-    if (total > 100){
-        showDiscount()
-    }
-}
-
-//Filtrar por los productos que sean prime.
-
-console.log("");
-console.log("");
-
+//We call the function to print in console
 for (i = 0; i < carrito.length; i++) {
-    if(carrito[i].premium){
-    console.log("Producto: " + (i+1))
+    imprimir (carrito[i]);
+}
+
+console.log("-------------------------------------")
+
+//    3    // Calculate the total price of the array.
+
+//We declare the variable we the total will be hosted
+let total = 0;
+
+//We create function to calculate total price per item and show in console
+
+function totalProduct (item){
+    console.log("The total price of " + item.name + " is: " + (item.count*item.price).toFixed(2) + " Euros");
+}
+
+//Create function to calculate the total price of the array
+
+function totalCarrito (item){
+    total+= item.count*item.price;
+}
+
+//Create function to show the total price of the array
+
+function showTotal(totalPrice) {
+    console.log("Total: " + totalPrice.toFixed(2) + " Euros")
+}
+
+//we create a loop, calling for the functions
+for (product of carrito){
+    totalProduct(product);
+    totalCarrito(product);
+}
+
+console.log("-------------------------------------")
+
+//Call the function showTotal to print the total trough console
+showTotal(total);
+
+console.log("-------------------------------------")
+
+//    4    // Filtrar por los productos que sean prime.
+//We simply use the type of variable of Premium (true/false) to create a condition and then print
+for (i = 0; i < carrito.length; i++) {
+    if(carrito[i].premium){    
     imprimir (carrito[i]);}
 }
-console.log("");
-console.log("");
 
-/* Si todos los productos son prime mostrar un mensaje "Pedido sin gastos de envío", si no "Este pedido
-tiene gastos de envío". */
+console.log("-------------------------------------")
 
+//    5    // If all products are Prime show >> "Pedido sin gastos de envío", if not >> "Este pedido tiene gastos de envío"
+
+//Create allprime to locate False value if during loop any object is not prime
 let allPrime = true;
 
-function listPrime(array){
+//Declare function where we will go every item to check if there is one which is not prime
+function listPrime (array){
 for (product of array){
     allPrime = allPrime && product.premium;
 }
 return allPrime;
 }
 
+//If the value of listPrime is true, it will mean all our products are prime, if false, then there is al least one that is not prime
 if (listPrime(carrito)){
     console.log("Pedido sin gastos de envío")
 }else{console.log("Este pedido tiene gastos de envío")}
 
-/* for (i = 0; i < carrito.length; i++){
-    document.getElementById("id1").innerHTML = carrito[i].id;
+//    6    // Aplicar 5% descuento si la compra es mayor a 100 euros
+
+//Create variable to alocate new price, if discount is applied
+let discountTotal = total;
+
+//Create function to recalculate total price if > 100
+function discount(){
+    if (total > 100){
+        discountTotal = 0.95*total;
+    }
+    return discountTotal;
 }
- */
 
+//Create function that will show the discount only if it is applied
 
+function showDiscount(){
+    if (total > 100){
+    console.log("5% descuento por compra superior a 100 euros");
+    console.log("El nuevo total es: " + discount().toFixed(2));
+}}
+
+showDiscount();
+
+//   7    // Crreate array with the id of HTML elements to relate with carrito array
+
+const htmlElements = [
+    {id: "id1", name: "name1", count: "count1", price: "price1", total: "total1", input: "prime1"},
+    {id: "id2", name: "name2", count: "count2", price: "price2", total: "total2", input: "prime2"},
+    {id: "id3", name: "name3", count: "count3", price: "price3", total: "total3", input: "prime3"},
+    {id: "id4", name: "name4", count: "count4", price: "price4", total: "total4", input: "prime4"},
+   
+    
+];
+
+//We relate the carrito properties with the DOM to show carrito in browser
+
+for (i = 0; i < htmlElements.length; i++) {
+document.getElementById(htmlElements[i].id).innerHTML = "ID: (" + carrito[i].id + ")";
+document.getElementById(htmlElements[i].name).innerHTML = carrito[i].name;
+document.getElementById(htmlElements[i].price).innerHTML = carrito[i].price;
+document.getElementById(htmlElements[i].total).innerHTML = (carrito[i].price*carrito[i].count).toFixed(2);
+document.getElementById(htmlElements[i].count).value = carrito[i].count;
+if (carrito[i].premium){
+document.getElementById(htmlElements[i].input).innerHTML = "Premium Item";
+}else{document.getElementById(htmlElements[i].input).style.display = "none";
+}};
+
+//We assign to html elements total bill price, the discounted price, and the shipping free info (if all elements are prime)
+
+document.getElementById("total-number").innerHTML = total.toFixed(2) + " €" ;
+document.getElementById("discount-total").innerHTML = "¡Precio con descuento! " + discountHtml() ;
+document.getElementById("prime-message").innerHTML = primeHtml() ;
+
+//We create function to show the discount price at the end, if there is discount
+
+function discountHtml(){
+    if (total > 100){
+        return discountTotal.toFixed(2);
+    }else{
+        return "";
+    }
+}
+
+function primeHtml(){
+    if(allPrime){
+        return "Pedido sin gastos de envío :-)"
+    }else{
+        return "Este pedido tiene gastos de envío :-("
+    }
+}
