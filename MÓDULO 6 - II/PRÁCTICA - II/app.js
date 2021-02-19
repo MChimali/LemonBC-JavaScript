8702308226073238498077e21449ee654dc3b9d3
Alpi <chimalimiguel@gmail.com>
2021-02-19 21:00:25 +0100

    simplify function

--- MÓDULO 6 - II/PRÁCTICA - II/app.js
+++ MÓDULO 6 - II/PRÁCTICA - II/app.js
@@ -9,46 +9,28 @@ document.getElementById("decrypt-button").addEventListener("click", showDecrypt)
 //Get value from upper text-area and pass it encrypted trough encryptText() to the lower text-area
 function showEncrypt () {
     var textToEncrypt = document.getElementById("decrypt-textarea").value;
-    document.getElementById("encrypt-textarea").value = encrypt(textToEncrypt.toLowerCase());
+    document.getElementById("encrypt-textarea").value = textChange(alphabet ,textToEncrypt.toLowerCase(), encryptedAlphabet);
     document.getElementById("decrypt-textarea").value = "";
 }
 
 //Get value from lower text-area and pass it decrypted trough decryptText() to the upper text-area
 function showDecrypt () {
     var textToDecrypt = document.getElementById("encrypt-textarea").value;
-    document.getElementById("decrypt-textarea").value = decrypt(textToDecrypt.toLowerCase());
+    document.getElementById("decrypt-textarea").value = textChange(encryptedAlphabet, textToDecrypt.toLowerCase(), alphabet);
     document.getElementById("encrypt-textarea").value = "";
 }
 
+//Iterates trough all letters of text, gettin their indexes, and directly passing the equivalent
+var textChange = (alphabetFrom, text, alphabetTo) => {
 
-//Iterates trough all letters of text, gettin their indexes, and directly passing the encrypted equivalent
-var encrypt = text => {
-
-    var encryptedText = ""; 
-
-    for (letter of text){
-        if (letter == " ")encryptedText += " ";
-        else{       
-        var index = alphabet.indexOf(letter);
-        encryptedText += encryptedAlphabet[index];
-        }  
-    }
-
-    return encryptedText;
-}
-
-//Iterates trough all letters of text, gettin their indexes, and directly passing the decrypted equivalent
-var decrypt = text => {
-
-    var decryptedText = ""; 
+    var textResult = ""; 
 
     for (letter of text){
-        if (letter == " ")decryptedText += " ";
+        if (letter === " ") textResult += " ";
         else{       
-        var index = encryptedAlphabet.indexOf(letter);
-        decryptedText += alphabet[index];
+        var index = alphabetFrom.indexOf(letter);
+        textResult += alphabetTo[index];
         }  
     }
-
-return decryptedText;
+    return textResult;
 }
